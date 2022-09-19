6b424c7710a3a6d77ed727b04de342495d0a09aa
amartinsmg <andremindurimg@gmail.com>
2022-09-19 11:28:15 -0300

    Update insertion_sort.hpp

--- src/insertion_sort.hpp
+++ src/insertion_sort.hpp
@@ -1,24 +1,22 @@
 #include <iostream>
+#include <cassert>
 
 template <typename T>
 T *insertion_sort(T *values, unsigned length)
 {
-  T num, *result = (T *)malloc(sizeof(*result) * length);
+  T num, *result = new T[length];
   unsigned i;
   int j;
+  assert(length > 0);
   result[0] = values[0];
   for (i = 1; i < length; i++)
   {
     result[i] = num = values[i];
-    for (j = i - 1; j >= 0; j--)
+    j = i;
+    while (--j >= 0 && num < result[j])
     {
-      if (num < result[j])
-      {
-        result[j + 1] = result[j];
-        result[j] = num;
-      }
-      else
-        break;
+      result[j + 1] = result[j];
+      result[j] = num;
     }
   }
   return result;
