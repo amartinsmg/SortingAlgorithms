d2801efdbbb496189184f634fa0b103bc608a0c6
amartinsmg <andremindurimg@gmail.com>
2022-09-24 11:09:51 -0300

    Update selection_sort.hpp

--- src/selection_sort.hpp
+++ src/selection_sort.hpp
@@ -4,22 +4,23 @@
 template <typename T>
 T *selection_sort(T *values, unsigned length)
 {
-  T num, *result = new T[length];
+  T *result = new T[length],
+    tmp;
   unsigned i, j;
   assert(length > 0);
   for (i = 0; i < length; i++)
     result[i] = values[i];
   for (i = 0; i < length; i++)
   {
-    num = result[i];
+    tmp = result[i];
     j = i;
     while (++j < length)
     {
-      if (result[j] < num)
+      if (result[j] < tmp)
       {
         result[i] = result[j];
-        result[j] = num;
-        num = result[i];
+        result[j] = tmp;
+        tmp = result[i];
       }
     }
   }
