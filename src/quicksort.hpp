bb218f07d9b95879463c7215b989b2a7f3b9abf9
andre <andremindurimg@gmail.com>
2023-06-30 17:37:20 -0300

    Update quicksort.hpp

--- src/quicksort.hpp
+++ src/quicksort.hpp
@@ -2,24 +2,24 @@
 #include <cstdlib>
 
 template <typename T>
-T *quicksort(T *values, unsigned length)
+T *quicksort(T *arr, int length)
 {
   T *result = new T[length],
     pivot, tmp;
-  unsigned **indexes = (unsigned **)malloc(sizeof(*indexes)),
-           indexesLength = 1,
-           i, j, k, low, high;
-  indexes[0] = (unsigned *)malloc(sizeof(**indexes) * 2);
-  indexes[0][0] = 0;
-  indexes[0][1] = length - 1;
+  int i, j, k, low, high,
+      **rangeIndexes = (int **)malloc(sizeof(*rangeIndexes)),
+      rangeIndexesLength = 1;
+  rangeIndexes[0] = (int *)malloc(sizeof(**rangeIndexes) * 2);
+  rangeIndexes[0][0] = 0;
+  rangeIndexes[0][1] = length - 1;
   assert(length > 0);
   for (i = 0; i < length; i++)
-    result[i] = values[i];
-  for (i = 0; i < indexesLength; i++)
+    result[i] = arr[i];
+  for (i = 0; i < rangeIndexesLength; i++)
   {
-    k = low = indexes[i][0];
-    high = indexes[i][1];
-    free(indexes[i]);
+    k = low = rangeIndexes[i][0];
+    high = rangeIndexes[i][1];
+    free(rangeIndexes[i]);
     if (low == high)
       continue;
     pivot = result[high];
@@ -36,19 +36,19 @@ T *quicksort(T *values, unsigned length)
     result[k] = pivot;
     if (k != low)
     {
-      j = indexesLength;
-      indexes = (unsigned **)realloc(indexes, sizeof(*indexes) * ++indexesLength);
-      indexes[j] = (unsigned *)malloc(sizeof(**indexes) * 2);
-      indexes[j][0] = low;
-      indexes[j][1] = k - 1;
+      j = rangeIndexesLength;
+      rangeIndexes = (int **)realloc(rangeIndexes, sizeof(*rangeIndexes) * ++rangeIndexesLength);
+      rangeIndexes[j] = (int *)malloc(sizeof(**rangeIndexes) * 2);
+      rangeIndexes[j][0] = low;
+      rangeIndexes[j][1] = k - 1;
     }
     if (k != high)
     {
-      j = indexesLength;
-      indexes = (unsigned **)realloc(indexes, sizeof(*indexes) * ++indexesLength);
-      indexes[j] = (unsigned *)malloc(sizeof(**indexes) * 2);
-      indexes[j][0] = k + 1;
-      indexes[j][1] = high;
+      j = rangeIndexesLength;
+      rangeIndexes = (int **)realloc(rangeIndexes, sizeof(*rangeIndexes) * ++rangeIndexesLength);
+      rangeIndexes[j] = (int *)malloc(sizeof(**rangeIndexes) * 2);
+      rangeIndexes[j][0] = k + 1;
+      rangeIndexes[j][1] = high;
     }
   }
 
