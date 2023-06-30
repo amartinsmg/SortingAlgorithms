ae9aac23a0c29a412e82fda7c978482b6a21fbde
andre <andremindurimg@gmail.com>
2023-06-30 17:53:44 -0300

    Update shellsort.hpp

--- src/shellsort.hpp
+++ src/shellsort.hpp
@@ -1,6 +1,16 @@
 #include <cassert>
 #include <cmath>
 
+/**
+  @brief Sorts an array in ascending order using the shell sort algorithm.
+  @tparam T The type of the array elements.
+  @param arr The input array to be sorted.
+  @param length The length of the array.
+  @return A new array containing the sorted elements.
+  @note The input array remains unchanged.
+  @note The caller is responsible for freeing the memory allocated for the returned array.
+*/
+
 template <typename T>
 T *shellsort(T *arr, int length)
 {
