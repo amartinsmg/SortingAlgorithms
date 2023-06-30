8aa560211054d05d1216b15226f4d67669234992
andre <andremindurimg@gmail.com>
2023-06-30 17:52:11 -0300

    Update bubblesort.hpp

--- src/bubblesort.hpp
+++ src/bubblesort.hpp
@@ -1,5 +1,16 @@
 #include <cassert>
 
+
+/**
+  @brief Sorts an array in ascending order using the bubble sort algorithm.
+  @tparam T The type of the array elements.
+  @param arr The input array to be sorted.
+  @param length The length of the array.
+  @return A new array containing the sorted elements.
+  @note The input array remains unchanged.
+  @note The caller is responsible for freeing the memory allocated for the returned array.
+*/
+
 template <typename T>
 T *bubblesort(T *arr, int length)
 {
