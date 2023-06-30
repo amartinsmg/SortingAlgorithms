73b0a420d782c1aad638d511a92003a9642387c0
andre <andremindurimg@gmail.com>
2023-06-30 17:36:59 -0300

    Update insertionsort.hpp

--- src/insertionsort.hpp
+++ src/insertionsort.hpp
@@ -1,16 +1,16 @@
 #include <cassert>
 
 template <typename T>
-T *insertionsort(T *values, unsigned length)
+T *insertionsort(T *arr, int length)
 {
   T *result = new T[length],
     tmp;
   int i, j;
   assert(length > 0);
-  result[0] = values[0];
+  result[0] = arr[0];
   for (i = 1; i < length; i++)
   {
-    result[i] = tmp = values[i];
+    result[i] = tmp = arr[i];
     j = i;
     while (--j >= 0 && tmp < result[j])
     {
