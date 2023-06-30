bbe61b45c79948263652ab65157d89b30beb9b69
andre <andremindurimg@gmail.com>
2023-06-30 17:53:22 -0300

    Update quicksort.hpp

--- src/quicksort.hpp
+++ src/quicksort.hpp
@@ -1,6 +1,16 @@
 #include <cassert>
 #include <cstdlib>
 
+/**
+  @brief Sorts an array in ascending order using the quick sort algorithm.
+  @tparam T The type of the array elements.
+  @param arr The input array to be sorted.
+  @param length The length of the array.
+  @return A new array containing the sorted elements.
+  @note The input array remains unchanged.
+  @note The caller is responsible for freeing the memory allocated for the returned array.
+*/
+
 template <typename T>
 T *quicksort(T *arr, int length)
 {
