eeeba41b26eb02e53b78d12b7a825e1b4d16149d
amartinsmg <andremindurimg@gmail.com>
2026-03-22 09:12:06 -0300

    refactor(insertion): use std::vector for array handling
    - Update function signature to use std::vector
    - Eliminate manual memory management for arrays
    - Remove responsibility for caller to free memory

--- include/insertionsort.hpp
+++ include/insertionsort.hpp
@@ -1,43 +1,42 @@
 #ifndef INSERTION_SORT_HPP
 #define INSERTION_SORT_HPP
 
-namespace Sort
-{
-  /**
-   * @brief Sorts an array in ascending order using the inserion sort algorithm.
-   * @tparam T The type of the array elements.
-   * @param arr The input array to be sorted.
-   * @param length The length of the array.
-   * @return A new array containing the sorted elements.
-   * @note The input array remains unchanged.
-   * @note The caller is responsible for freeing the memory allocated for the returned array.
-   */
+#include <cstddef>
+#include <cstdint>
+#include <vector>
 
-  template <typename T>
-  static inline T *insertionsort(T *arr, int length)
-  {
-    if (length <= 0 || arr == nullptr)
-      return nullptr;
+namespace Sort {
+/**
+ * @brief Sorts an array in ascending order using the insertion sort algorithm.
+ * @tparam T The type of the array elements.
+ * @param arr The input array to be sorted.
+ * @return A new array containing the sorted elements.
+ * @note The input array remains unchanged.
+ */
 
-    T tmp,
-        *result = new T[length];
-    int i, j;
-
-    result[0] = arr[0];
-
-    for (i = 1; i < length; i++)
-    {
-      result[i] = tmp = arr[i];
-      j = i;
-      while (--j >= 0 && tmp < result[j])
-      {
-        result[j + 1] = result[j];
-        result[j] = tmp;
-      }
-    }
-    return result;
+template <typename T>
+static inline std::vector<T> insertionsort(const std::vector<T> arr) {
+  if (arr.size() == 0) {
+    std::vector<T> v(0);
+    return v;
   }
 
-} // Sort
+  std::vector<T> result(arr);
+  T tmp;
+  size_t i;
+  int64_t j;
+
+  for (i = 1; i < arr.size(); i++) {
+    tmp = result[i];
+    j = i - 1;
+    while (j >= 0 && tmp < result[j]) {
+      result[j + 1] = result[j];
+      result[j--] = tmp;
+    }
+  }
+  return result;
+}
+
+} // namespace Sort
 
 #endif /* INSERTION_SORT_HPP */
