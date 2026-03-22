93a2c06200ea0e099652a35031d5aad4adc38302
amartinsmg <andremindurimg@gmail.com>
2026-03-22 09:21:34 -0300

    style(sort): update include directives

--- include/sort.hpp
+++ include/sort.hpp
@@ -1,12 +1,12 @@
 #ifndef SORT_HPP
 #define SORT_HPP
 
-#include <bubblesort.hpp>
-#include <heapsort.hpp>
-#include <insertionsort.hpp>
-#include <mergesort.hpp>
-#include <quicksort.hpp>
-#include <selectionsort.hpp>
-#include <shellsort.hpp>
+#include "bubblesort.hpp"
+#include "heapsort.hpp"
+#include "insertionsort.hpp"
+#include "mergesort.hpp"
+#include "quicksort.hpp"
+#include "selectionsort.hpp"
+#include "shellsort.hpp"
 
 #endif /* SORT_HPP */
