38d30f909d2a9216023610371ee99be3e780149b
amartinsmg <andremindurimg@gmail.com>
2026-03-22 09:11:34 -0300

    refactor(heapsort): use std::vector for array handling
    - Update function signature to use std::vector
    - Eliminate manual memory management for arrays
    - Remove responsibility for caller to free memory

--- include/heapsort.hpp
+++ include/heapsort.hpp
@@ -1,63 +1,55 @@
 #ifndef HEAP_SORT_HPP
 #define HEAP_SORT_HPP
 
-namespace Sort
-{
-  /**
-   * @brief Sorts an array in ascending order using the heap sort algorithm.
-   * @tparam T The type of the array elements.
-   * @param arr The input array to be sorted.
-   * @param length The length of the array.
-   * @return A new array containing the sorted elements.
-   * @note The input array remains unchanged.
-   * @note The caller is responsible for freeing the memory allocated for the returned array.
-   */
+#include <cstdint>
+#include <vector>
 
-  template <typename T>
-  static inline T *heapsort(T *arr, int length)
-  {
-    if (length <= 0 || arr == nullptr)
-      return nullptr;
+namespace Sort {
+/**
+ * @brief Sorts an array in ascending order using the heap sort algorithm.
+ * @tparam T The type of the array elements.
+ * @param arr The input array to be sorted.
+ * @return A new array containing the sorted elements.
+ * @note The input array remains unchanged.
+ */
 
-    T tmp,
-        *result = new T[length];
-    int i, j, left, right,
-        heapLength = length;
-
-    for (i = 0; i < length; i++)
-      result[i] = arr[i];
-
-    while (heapLength > 1)
-    {
-      for (i = heapLength / 2; i >= 0; i--)
-      {
-        for (j = i; j < heapLength / 2; j++)
-        {
-          left = 2 * j + 1;
-          right = left + 1;
-          if (left < heapLength && result[j] < result[left])
-          {
-            tmp = result[j];
-            result[j] = result[left];
-            result[left] = tmp;
-          }
-          if (right < heapLength && result[j] < result[right])
-          {
-            tmp = result[j];
-            result[j] = result[right];
-            result[right] = tmp;
-          }
-        }
-      }
-
-      tmp = result[--heapLength];
-      result[heapLength] = result[0];
-      result[0] = tmp;
-    }
-
-    return result;
+template <typename T>
+static inline std::vector<T> heapsort(const std::vector<T> arr) {
+  if (arr.size() == 0) {
+    std::vector<T> v(0);
+    return v;
   }
 
-} // Sort
+  std::vector<T> result = arr;
+  T tmp;
+  int64_t i, j, left, right, heapLength = arr.size();
+
+  while (heapLength > 1) {
+    for (i = heapLength / 2; i >= 0; i--) {
+      for (j = i; j < heapLength / 2; j++) {
+        left = 2 * j + 1;
+        right = left + 1;
+        if (left < heapLength && result[j] < result[left]) {
+          tmp = result[j];
+          result[j] = result[left];
+          result[left] = tmp;
+        }
+        if (right < heapLength && result[j] < result[right]) {
+          tmp = result[j];
+          result[j] = result[right];
+          result[right] = tmp;
+        }
+      }
+    }
+
+    tmp = result[--heapLength];
+    result[heapLength] = result[0];
+    result[0] = tmp;
+  }
+
+  return result;
+}
+
+} // namespace Sort
 
 #endif /* HEAP_SORT_HPP */
